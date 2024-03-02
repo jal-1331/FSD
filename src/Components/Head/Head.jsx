3e3fb4dfc7273b6d577377cb6069ed472a03f9e2
jal-1331 <jalpatel110504@gmail.com>
2024-03-02 16:52:56 +0530

    frontend nav

--- src/Components/Head/Head.jsx
+++ src/Components/Head/Head.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';  // Import Link from react-router-dom
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Head.css';
 
 const Head = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleToggle = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
       <header className="header">
         <nav className="nav">
-          
-          <Link to="/" className="nav_logo">Hobbies</Link>  {/* Replace "a" with "Link" */}
-          <ul className="nav_items">
+          <Link to="/" className="nav_logo">Hobbies</Link>
+          <input
+            type="checkbox"
+            id="menu-toggle"
+            className="menu-toggle"
+            checked={isMenuOpen}
+            onChange={handleToggle}
+          />
+          <label htmlFor="menu-toggle" className="menu-icon">&#9776;</label>
+          <ul className={`nav_items ${isMenuOpen ? 'show' : ''}`}>
             <li className="nav_item">
-              <Link to="/EventDetails" className="nav_link">Home</Link>  {/* Replace "a" with "Link" */}
-              <Link to="#" className="nav_link">About</Link>  {/* Add correct route */}
-              <Link to="#" className="nav_link">Contact</Link>  {/* Add correct route */}
-              <Link to="/Profile" className="nav_link">MyAccount</Link>  {/* Add correct route */}
+              <Link to="/EventDetails" className="nav_link">Home</Link>
+              <Link to="#" className="nav_link">About</Link>
+              <Link to="#" className="nav_link">Contact</Link>
+              <Link to="/Profile" className="nav_link">MyAccount</Link>
             </li>
           </ul>
         </nav>
